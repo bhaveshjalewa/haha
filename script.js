e8a3174da87e53f6ed2b710568ee325e0e20a60d
bhaveshjalewa <bhaveshjalewa@gmail.com>
2026-03-05 20:09:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,10 +168,8 @@ function submitPuzzle(){
 /* ================= 52 CHAR CODE ================= */
 
 function generateCode(){
-  const chars="Z9@vL#3xT!7qR$2mP^8yW*4kB%1dF&6nH?0cJ+5sU=E!aX";
-  let code="";
-  for(let i=0;i<52;i++);
-  }
+  const chars=
+"Z9@vL#3xT!7qR$2mP^8yW*4kB%1dF&6nH?0cJ+5sU=E!aX";
   return code;
 }
 
